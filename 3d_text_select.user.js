68d9f0b501037f6b4e97ff6c590f3d17c87c992e
toddmath <tmatheson11186@gmail.com>
2019-12-10 21:46:01 -0800

    Major refactor of code and cleanup. Changed to ES6 javascript and improved code to insert only a single style tag inside the head tag

--- 3d_text_select.user.js
+++ 3d_text_select.user.js
@@ -39,56 +39,56 @@
 // @connect      *
 // @include      *
 // @run-at       document-end
-// @updateURL    https://openuserjs.org/meta/toddmath/3d_Text_Select.meta.js
-// @require      https://code.jquery.com/jquery-3.4.1.slim.min.js#sha256=pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8
+// @updateURL    https://openuserjs.org/meta/toddmath/3d_text_select.meta.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js#sha256=pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8
 // ==OpenUserJS==
 // @author       toddmath
 // ==/OpenUserJS==
 // ==/UserScript==
-'use strict'
+"use strict";
 
 //Avoid conflicts
 this.$ = this.jQuery = jQuery.noConflict(true);
-$(document).ready(function () {
+$(document).ready(function() {
   // Customize text shadow below (must be valid css string)
-  var cus_text_shadow = "1px 1px 1px #000, 2px 2px 2px #5C6BC0, 3px 3px 3px #536DFE, 3px 3px 3px #7986CB, 4px 4px 5px #EA80FE, 5px 5px 7px #E1BEE7";
+  const cus_text_shadow =
+    "1px 0px 1px #424242, 0px 1px 1px #424242, 0px -1px 1px #424242, -1px 0px 1px #424242, 1px 1px 1px #5C6BC0, 2px 2px #536DFE, 3px 3px 1px #7986CB, 4px 4px 2px #EA80FE, 4px 5px 2px #E1BEE7";
+  // Alternative custom text shadow
+  var cus_text_shadow_alt =
+    "0 0 0 #000, 0 0 0 #D7624E, 0.0025em 0.0025em 0 #D7624E, 0.005em 0.005em 0 #D7624E, 0.0075em 0.0075em 0 #D7624E, 0.01em 0.01em 0 #D7624E, 0.0125em 0.0125em 0 #D7624E, 0.015em 0.015em 0 #D7624E, 0.0175em 0.0175em 0 #D7624E, 0.02em 0.02em 0 #D7624E, 0.0225em 0.0225em 0 #D7624E, 0.025em 0.025em 0 #D7624E, 0.0275em 0.0275em 0 #D7624E, 0.03em 0.03em 0 #D7624E, 0.0325em 0.0325em 0 #D7624E, 0.035em 0.035em 0 #D7624E, 0.0375em 0.0375em 0 #D7624E, 0.04em 0.04em 0 #D7624E, 0.0425em 0.0425em 0 #D7624E, 0.045em 0.045em 0 #D7624E, 0.0475em 0.0475em 0 #D7624E, 0.05em 0.05em 0 #D7624E, 0.0525em 0.0525em 0 #D7624E, 0.055em 0.055em 0 #D7624E, 0.0575em 0.0575em 0 #D7624E, 0.06em 0.06em 0 #D7624E, 0.0625em 0.0625em 0 #D7624E, 0.065em 0.065em 0 #D7624E, 0.0675em 0.0675em 0 #D7624E, 0.07em 0.07em 0 #D7624E, 0.0725em 0.0725em 0 #D7624E, 0.075em 0.075em 0 #D7624E, 0.0775em 0.0775em 0 #D7624E, 0.08em 0.08em 0 #D7624E, 0.0825em 0.0825em 0 #D7624E, 0.085em 0.085em 0 #D7624E, 0.0875em 0.0875em 0 #D7624E, 0.09em 0.09em 0 #D7624E, 0.0925em 0.0925em 0 #D7624E, 0.095em 0.095em 0 #D7624E, 0.0975em 0.0975em 0 #D7624E, 0.1em 0.1em 0 #D7624E;";
   // Customize color of selected text below
-  var cus_text_color = "#FAFAFA";
+  const cus_text_color = "#FAFAFA";
+  // Alternative selected text color
+  const cus_text_color_alt = "#424242";
   // Customize background below
-  var cus_background = "none repeat scroll 0% 0% transparent";
+  const cus_background = "none repeat scroll 0% 0% transparent";
   // Customize input field text color (selected text inside form fields and other text-fields)
-  var cus_input_text_color = "#536DFE";
+  const cus_input_text_color = "#536DFE";
   // Customize 3d level (selected text moving up)
-  var cus_top = "-3px";
+  const cus_top = "0.0975em !important";
   // Customize 3d level (selected text moving left)
-  var cus_left = "-3px";
+  const cus_left = "0.0975em !important";
+  const text_grow = "20% !important";
+  // Hack to render selected text using GPU acceleration (faster & smoother)
+  const gpu_render = "translateZ(0, 0, 0) !important";
 
-  function universal_select_style() {
-    GM_addStyle(`input::selection { position: relative; color: ${cus_input_text_color} !important; background: ${cus_background} };`)
-    GM_addStyle(`a::selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-    GM_addStyle(`textarea::selection { position: relative; color: ${cus_input_text_color} !important; background: ${cus_background} };`)
-  }
+  const inputSelectors = ["input::selection", "textarea::selection"];
+  const browserSelectors = ["a::selection", "::selection", "::-webkit-selection", "::-ms-selection", "::-o-selection"];
 
-  function firefox_select_style() {
-    GM_addStyle(`::-moz-selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-  }
+  const browserRules = `position: relative; top: ${cus_top}; left: ${cus_left}; color: ${cus_text_color_alt}; background: ${cus_background}; text-shadow: ${cus_text_shadow_alt};`;
 
-  function chrome_select_style() {
-    GM_addStyle(`::selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-  }
+  const inputRules = `position: relative; color: ${cus_input_text_color} !important; background: ${cus_background};`;
 
-  function webkit_select_style() {
-    GM_addStyle(`::-webkit-selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-  }
+  let styleString = "";
 
-  function other_select_style() {
-    GM_addStyle(`::-ms-selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-    GM_addStyle(`::-o-selection { position: relative; top: ${cus_top} ; left: ${cus_left}; color: ${cus_text_color}; background: ${cus_background}; text-shadow: ${cus_text_shadow}; }`)
-  }
+  const poolStyles = (select, styles) => `${select} {${styles}}`;
 
-  universal_select_style();
-  firefox_select_style();
-  chrome_select_style();
-  webkit_select_style();
-  other_select_style();
+  const addStyles = str => GM_addStyle(String(str));
+
+  inputSelectors.forEach(sel => styleString += poolStyles(sel, inputRules));
+  browserSelectors.forEach(
+    sel => (styleString += poolStyles(sel, browserRules))
+  );
+
+  addStyles(styleString);
 });
